157f71d7141b90612dc842ea22dad58e24cd8aeb
celesteornato <celeste.ornato1@gmail.com>
2024-08-17 16:52:44 +0200

    The year of the desktop linux

--- nixos/thinkpad.nix
+++ nixos/thinkpad.nix
@@ -1,3 +1,4 @@
 {pkgs, ...}: {
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  services.xserver.displayManager.gdm.enable = true; # The Dell has an Nvidia and cannot boot into wayland from gdm
 }
