c5ad1c023ca3f85342858043200be8eb6008a0cb
celesteornato <celeste.ornato1@gmail.com>
2024-11-08 02:31:11 +0100

    Changed rebuild script

--- scripts/nx-switch.nix
+++ scripts/nx-switch.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 let
   switch = pkgs.writeShellScriptBin "switch" ''
-    sudo nixos-rebuild switch --flake "$HOME/dotfiles" --impure
+    sudo nixos-rebuild switch --flake "$HOME/dotfiles"#"nixos-tp" --impure
   '';
   upgrade = pkgs.writeShellScriptBin "upgrade" ''
     nix flake update
-    sudo nixos-rebuild switch --flake "$HOME/dotfiles" --impure
+    sudo nixos-rebuild switch --flake "$HOME/dotfiles"#"nixos-tp" --impure
   '';
 in
 {
