b86a9cc51f6285bee467adf983dedd67399313c4
Main <main@nc-ass-vip.sdv.fr>
2025-04-17 22:06:26 +0200

    made cool config

--- config/waybar/weather.sh
+++ config/waybar/weather.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 LOCATION=$(curl -s 'http://ip-api.com/json?fields=city' | jq '.city' | sed 's/.$//' | cut -c 2-)
 
-echo $(ansiweather -l "$LOCATION" -p false -a false -i false -H true)
+echo $(~/.config/waybar/ansiweather -l "$LOCATION" -p false -a false -i false -H true)
