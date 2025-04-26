fa844073f4c968f889039b5cf841c934b4e42d4a
Céleste Ornato <celeste@ornato.com>
2025-04-26 22:53:30 +0200

    new waybar who dis

--- config/waybar/weather.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-LOCATION=$(curl -s 'http://ip-api.com/json?fields=city' | jq '.city' | sed 's/.$//' | cut -c 2-)
-
-echo $(~/.config/waybar/ansiweather -l "$LOCATION" -p false -a false -i false -H true)
