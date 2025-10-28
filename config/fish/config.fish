134348df78fd103192a1c3f41d586c624172c6f0
Céleste Ornato <celeste@ornato.com>
2025-10-28 10:57:46 +0100

    fish: removed mail & added ~/.bin to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,4 @@
 if status is-interactive
     source ~/.config/fish/manpages.fish
-
-    mail -e
-    switch "$status"
-        case 0
-            echo "You have mail"
-    end
 end
 set fish_greeting
