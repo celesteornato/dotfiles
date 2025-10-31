3742c8753faea78e62d11ba858d767fccdb59493
Céleste Ornato <celeste@ornato.com>
2025-10-31 04:17:27 +0100

    waybar: made "light bg mode" transparent

--- config/sway/scripts/bg_switch.sh
+++ config/sway/scripts/bg_switch.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 cp "$1" ~/.background_img
-swaymsg reload &
+swaymsg reload
+
 BRIGHTNESS=$(magick ~/.background_img -gravity South -chop 90% -colorspace gray -format "%[fx:round(100*mean)]" info:)
-unlink ~/.config/waybar/current.css
-if [ $BRIGHTNESS -gt 20 ];
+
+if [ $BRIGHTNESS -gt 40 ]
 then
   echo "switching to light bg mode"
-  ln -s ~/.config/waybar/light.css ~/.config/waybar/current.css
+  swaymsg 'layer_effects "waybar" blur disable;'
+  ln -f ~/.config/waybar/light.css ~/.config/waybar/current.css
 else
   echo "switching to dark bg mode"
-  ln -s ~/.config/waybar/dark.css ~/.config/waybar/current.css
+  swaymsg 'layer_effects "waybar" blur enable;'
+  ln -f ~/.config/waybar/dark.css ~/.config/waybar/current.css
 fi
-pkill waybar
-exec waybar & 
+
+pkill waybar -USR2
