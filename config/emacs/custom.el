53f0e88ed8658547aa35e51118d134d5762a2df7
Céleste Ornato <celeste@ornato.com>
2025-11-23 23:14:11 +0100

    custom: added unselected packages that were assumed to be present

--- config/emacs/custom.el
+++ config/emacs/custom.el
@@ -5,7 +5,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
+   '("5a0ddbd75929d24f5ef34944d78789c6c3421aa943c15218bac791c199fc897d"
+     "8363207a952efb78e917230f5a4d3326b2916c63237c1f61d7e5fe07def8d378"
+     "51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3"
+     "d5fd482fcb0fe42e849caba275a01d4925e422963d1cd165565b31d3f4189c87"
+     "01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
      default))
  '(evil-undo-system 'undo-tree)
  '(global-undo-tree-mode t)
@@ -19,10 +23,10 @@
 		 1.0 :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(org-startup-with-latex-preview t)
  '(package-selected-packages
-   '(company company-box doom-modeline evil evil-smartparens flycheck
-	     format-all gruber-darker-theme magit mistty org-bullets
-	     org-fragtog org-modern org-superstar quelpa
-	     quelpa-use-package smartparens undo-tree)))
+   '(auctex blamer cdlatex company-box doom-modeline evil-smartparens
+	    flycheck format-all gruber-darker-theme magit mistty
+	    org-bullets org-fragtog org-modern org-modern-indent
+	    org-superstar quelpa-use-package undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
