53c3539de1d585eeebf78482bf1d0be4c37a7a3c
Céleste Ornato <celeste@ornato.com>
2025-11-26 00:34:20 +0100

    phone_battery: large fixes

--- config/waybar/scripts/phone_battery.sh
+++ config/waybar/scripts/phone_battery.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
-if ! pgrep -x "kdeconnectd" > /dev/null; then
+PHONEPATH="/modules/kdeconnect/devices/$PHONEID"
+
+if [ "$(pgrep -x "kdeconnectd")" = "" ] ; then
+    echo "{ \"text\" : \"\", \"class\" : \"disconnected\"  }"
     exit 1
 fi
 
-if qdbus org.kde.kdeconnect /modules/kdeconnect/devices/$PHONEID org.kde.kdeconnect.device.isReachable | grep true > /dev/null; then
-    echo $(qdbus org.kde.kdeconnect /modules/kdeconnect/devices/$PHONEID/battery org.kde.kdeconnect.device.battery.charge)%
+if [ "$(qdbus org.kde.kdeconnect $PHONEPATH org.kde.kdeconnect.device.isReachable)" = "false" ] ; then
+    echo "{ \"text\" : \"\", \"class\" : \"disconnected\"  }"
+    exit 1
 fi
+
+CHARGE=$(qdbus org.kde.kdeconnect "$PHONEPATH/battery" org.kde.kdeconnect.device.battery.charge)
+DEVNAME=$(qdbus org.kde.kdeconnect "$PHONEPATH" org.kde.kdeconnect.device.name)
+CHARGING=$(qdbus org.kde.kdeconnect "$PHONEPATH/battery" org.kde.kdeconnect.device.battery.isCharging)
+
+TEXT="Connected to $DEVNAME ($CHARGE% Battery)"
+TEXT_CHARGING="Connected to $DEVNAME ($CHARGE% Battery)"
+
+if [ "$CHARGING" = "true" ] ; then
+    echo "{ \"text\" : \"$TEXT\", \"class\" : \"charging\"  }"
+fi
+
+if [ "$CHARGING" = "false" ] ; then
+    echo "{ \"text\" : \"$TEXT\", \"class\" : \"normal\"  }"
+fi
+
